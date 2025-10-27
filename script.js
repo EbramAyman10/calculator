56f7703f644207eec8466d1c936a78da347b4dc9
Ebram Ayman <ebramayman10@gmail.com>
2025-10-27 19:57:53 +0300

    updating project

---
+++ script.js
@@ -0,0 +1,32 @@
+const display = document.querySelector(".js-text");
+const buttons = document.querySelectorAll(".js-btn");
+
+display.innerText = localStorage.getItem("result") || "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.innerText;
+
+    if (value === "Clear") {
+      display.innerHTML = "";
+      localStorage.removeItem("result");
+    } else if (value === "=") {
+      display.innerText = eval(display.innerText);
+      localStorage.setItem("result", display.innerText);
+    } else {
+      const operators = ["+", "-", "*", "/"];
+      const lastchar = display.innerText.slice(-1);
+      if (operators.includes(value)) {
+        if (operators.includes(lastchar)) {
+          display.innerText = display.innerText.slice(0, -1) + value;
+          localStorage.setItem("result", display.innerText);
+
+          return;
+        }
+        if (display.innerText === "") return;
+      }
+      display.innerText += value;
+    }
+    localStorage.setItem("result", display.innerText);
+  });
+});
